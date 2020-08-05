73cd8b8b2d13937ca23f2b59001bbd0fc24411aa
Ajtheult <asaphfranksboi@gmail.com>
2020-08-05 11:06:24 +0300

    final

---
+++ .history/models/Contact_20200805110230.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("contact", ContactSchema);
